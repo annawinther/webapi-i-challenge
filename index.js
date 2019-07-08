8ec9fee6eac6a258215051c797b569130bca07ce
Anna W <anski.anna@gmail.com>
2019-07-08 13:24:09 +0100

    removed the new implemented server, started with endpoints

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-// implement your API here
-// built in node.js module to handle http traffic
-const http = require('http');
-// the local computer where the server is running
-const hostname = '127.0.0.1';
-// a port we'll use to watch for traffic
-const port = 3001;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from Node\n');
-})
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-})
-
+// // implement your API here
 
 // step one: bring express and create an express app
 const express = require('express');
@@ -28,8 +11,15 @@ const Hub = require('./data/db');
 server.use(express.json());
 
 // step three: create endpoints
-
-
+server.get('/api/users', (req, res) => {
+    Hub.find()
+        .then(data => {
+            console.log('happy');
+        })
+        .catch(err => {
+            console.log('sad')
+        })
+})
 
 
 
